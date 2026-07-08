32e7429ea6c573d670ed59efc9104fe7e69d0144
Okelola Joshua <jayokelola341@gmail.com>
2026-07-08 23:55:17 +0100

    feat: add base wallet repository class

---
+++ lib/features/transactions/domain/wallet_repo.dart
@@ -0,0 +1,19 @@
+import 'package:eazywallet/features/transactions/domain/report_transaction_model.dart';
+import 'package:eazywallet/features/transactions/domain/transaction_model.dart';
+import 'package:eazywallet/features/transactions/domain/user_model.dart';
+
+abstract class WalletRepository {
+  Future<UserModel> fetchDashboard();
+
+  Future<List<TransactionModel>> fetchTransactions();
+
+  Future<TransactionModel> fetchTransactionById(String id);
+
+  Future<ReportTransactionModel> submitTransactionReport({
+    required String transactionId,
+    required ReportReason reason,
+    required String description,
+  });
+
+  Future<bool> verifyTransactionPin(String pin);
+}
